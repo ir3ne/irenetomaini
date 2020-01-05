0e28b8e3d9dd25ea8bb736d93065e30e3799c460
irene <irene.tomaini@gmail.com>
2020-01-06 00:01:28 +0100

    add content text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="Name">Irene Tomaini</div>
+      <div className="Text">
+      Irene Tomaini is a Front-end developer specializing in UI, CSS, accessibility.
+      <br />
+      Scss enthusiast.
+      <br />
+      She works remotely from Italy.
+      </div>
+      <div className="Contacts">
+        <div className="Contacts-link">
+          <a href="https://www.linkedin.com/in/irenetomaini/">Linkedin</a>
+        </div>
+        <div className="Contacts-link">
+          <a href="https://github.com/ir3ne">Github</a>
+        </div>
+        <div className="Contacts-link">
+          <a href="https://twitter.com/irenetomaini1">Twitter</a>
+        </div>
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +37,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
