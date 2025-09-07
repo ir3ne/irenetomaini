c6fc411d77d56d6eeb9f7785f8fb7f69380d5005
Irene Tomaini <irene.tomaini@gmail.com>
2025-09-07 17:16:57 +0200

    fix style for larger screens

--- src/Loader.tsx
+++ src/Loader.tsx
@@ -1,5 +1,16 @@
 const Loader = () => {
-  return <div className="loader">Loading...</div>;
+  return (
+    <div className="loader">
+      <div className="wave-loader">
+        <div className="wave-bar"></div>
+        <div className="wave-bar"></div>
+        <div className="wave-bar"></div>
+        <div className="wave-bar"></div>
+        <div className="wave-bar"></div>
+      </div>
+      <p className="loader-text">Loading repositories...</p>
+    </div>
+  );
 };
 
 export default Loader;
